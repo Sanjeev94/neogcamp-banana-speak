5423de67b9b81a5a520c2d470804b6cc1c1b074a
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-08-15 17:12:36 +0530

    import javascript

---
+++ apps.js
@@ -0,0 +1,8 @@
+console.log("Script is working from a different file")
+
+// alert("this script works")
+
+var username = prompt("give me ur username?")
+
+var welcomeMessage = "this script works!" + username;
+alert(welcomeMessage);
