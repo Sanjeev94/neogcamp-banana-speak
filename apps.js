29fc283ae14d7c3ac763181321636c6678630204
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-08-15 18:10:42 +0530

    taking input

--- apps.js
+++ apps.js
@@ -1,8 +1,27 @@
-console.log("Script is working from a different file")
+// console.log("Script is working from a different file")
 
-// alert("this script works")
+// // alert("this script works")
 
-var username = prompt("give me ur username?")
+// var username = prompt("give me ur username?")
 
-var welcomeMessage = "this script works!" + username;
-alert(welcomeMessage);
+// var welcomeMessage = "this script works!" + username;
+// alert(welcomeMessage);
+
+var btnTranslate =document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+// btnTranslate.addEventListener("click", function clickEventHandler() {
+//     console.log("clicked")
+// });
+
+//callback
+function clickHandler() {
+    console.log("clicked")
+    console.log("input", txtInput.value)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
+
+//wiring textarea input
+
